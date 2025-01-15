5e799c7411907ef772f2892c15b0c60ddaf7e2de
Luis Miguel Casado Díaz <luismiguelcasadodiaz@gmail.com>
2025-01-15 23:32:09 +0100

    Feat: ClapTrap OK

---
+++ ex00/main.cpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luicasad <luicasad@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/15 19:38:03 by luicasad          #+#    #+#             */
+/*   Updated: 2025/01/15 22:52:45 by luicasad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.class.hpp"
+int main( void )
+{
+	ClapTrap a;
+	std::cout << a << std::endl;
+	a.attack("hola");
+	std::cout << a << std::endl;	
+	a.takeDamage(1);
+	std::cout << a << std::endl;
+	a.takeDamage(1);
+	std::cout << a << std::endl;	
+	a.takeDamage(1);
+	std::cout << a << std::endl;	
+	a.takeDamage(1);
+	std::cout << a << std::endl;	
+	a.beRepaired(321);
+	std::cout << a << std::endl;
+
+	a = ClapTrap("DELL");
+	std::cout << a << std::endl;	
+
+	return (0);
+}
